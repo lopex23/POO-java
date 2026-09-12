b67b1d6da68b8ad48a8c4ef1ea8fbf559a6d10a1
lopex23 <ssebastian2311lg@gmail.com>
2026-09-11 21:44:41 -0500

    Create Persona class with attributes and constructor

---
+++ src/Persona.java
@@ -0,0 +1,17 @@
+package POO;
+
+public class Persona {
+
+    public String nombre;
+    public String apellido;
+    public int edad;
+    public String identificacion;
+
+    public Persona(String nombre, String apellido, int edad, String identificacion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.identificacion = identificacion;
+    }
+
+}
