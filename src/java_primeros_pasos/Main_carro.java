3eb505723ff75aba1315821988ad200f279d6406
lopex23 <ssebastian2311lg@gmail.com>
2026-09-11 21:48:16 -0500

    Create Main_carro.java to instantiate Carro objects

---
+++ src/java_primeros_pasos/Main_carro.java
@@ -0,0 +1,39 @@
+package POO;
+
+public class Main_carro {
+
+    public static void main(String[] args) {
+
+
+        Carro c1 = new Carro("Toyota", "Prado TXL", "Blanco", "Gasolina", 200, 0, 18, "Japon", "ABC123", "Automatica");
+        Carro c2 = new Carro("Kia", "Rio", "Negro", "Gasolina", 185, 0, 15, "Corea", "DEF456", "Manual");
+        Carro c3 = new Carro("Chevrolet", "Onix", "Rojo", "Gasolina", 180, 0, 15, "EEUU", "GHI789", "Manual");
+        Carro c4 = new Carro("Mazda", "3", "Azul", "Gasolina", 210, 0, 18, "Japon", "JKL012", "Automatica");
+        Carro c5 = new Carro("Renault", "Duster", "Gris", "Gasolina", 175, 0, 16, "Francia", "MNO341", "Manual");
+        Carro c6 = new Carro("Nissan", "Versa", "Plata", "Gasolina", 180, 0, 15, "Japon", "PQR678", "Automatica");
+        Carro c7 = new Carro("Ford", "Mustang GT", "Amarillo", "Gasolina", 250, 0, 19, "EEUU", "STU901", "Automatica");
+        Carro c8 = new Carro("Volkswagen", "Golf GTi", "Blanco", "Gasolina", 240, 0, 18, "Alemania", "VWX234", "Automatica");
+        Carro c9 = new Carro("BMW", "M3", "Azul", "Gasolina", 290, 0, 19, "Alemania", "YZA567", "Automatica");
+        Carro c10 = new Carro("Mercedes-Benz", "Clase C", "Negro", "Hibrido", 250, 0, 18, "Alemania", "BCD890", "Automatica");
+        Carro c11 = new Carro("Audi", "A4", "Gris", "Gasolina", 240, 0, 18, "Alemania", "EFG123", "Automatica");
+        Carro c12 = new Carro("Hyundai", "Tucson", "Rojo", "Gasolina", 190, 0, 17, "Corea", "HIJ456", "Automatica");
+        Carro c13 = new Carro("Honda", "Civic", "Blanco", "Gasolina", 200, 0, 17, "Japon", "KLM789", "CVT");
+        Carro c14 = new Carro("Suzuki", "Jimny", "Verde", "Gasolina", 145, 0, 15, "Japon", "NOP012", "Manual");
+        Carro c15 = new Carro("Jeep", "Wrangler", "Naranja", "Gasolina", 180, 0, 17, "EEUU", "QRS345", "Automatica");
+        Carro c16 = new Carro("Subaru", "WRX", "Azul", "Gasolina", 235, 0, 18, "Japon", "TUV678", "Manual");
+        Carro c17 = new Carro("Tesla", "Model 3", "Blanco", "Electrico", 225, 0, 18, "EEUU", "WXY901", "Automatica");
+        Carro c18 = new Carro("BYD", "Dolphin", "Rosa", "Electrico", 160, 0, 16, "China", "ZAB234", "Automatica");
+        Carro c19 = new Carro("Porsche", "911 Carrera", "Gris", "Gasolina", 293, 0, 20, "Alemania", "CDE567", "Automatica");
+        Carro c20 = new Carro("Ferrari", "488 GTB", "Rojo", "Gasolina", 330, 0, 20, "Italia", "FGH890", "Automatica");
+        Carro c21 = new Carro("Lamborghini", "Huracan", "Amarillo", "Gasolina", 325, 0, 20, "Italia", "IJK123", "Automatica");
+        Carro c22 = new Carro("Volvo", "XC90", "Negro", "Hibrido", 180, 0, 20, "Suecia", "LMN456", "Automatica");
+        Carro c23 = new Carro("Peugeot", "208", "Azul", "Gasolina", 190, 0, 16, "Francia", "OPQ789", "Manual");
+        Carro c24 = new Carro("Fiat", "Uno", "Blanco", "Gasolina", 150, 0, 13, "Italia", "RST012", "Manual");
+        Carro c25 = new Carro("Seat", "Ibiza", "Rojo", "Gasolina", 195, 0, 16, "Espana", "UVW345", "Manual");
+        Carro c26 = new Carro("Mini", "Cooper S", "Verde", "Gasolina", 235, 0, 17, "Reino Unido", "XYZ678", "Automatica");
+        Carro c27 = new Carro("Mitsubishi", "L200", "Plata", "Diesel", 170, 0, 17, "Japon", "ABC901", "Manual");
+        Carro c28 = new Carro("Ram", "1500", "Negro", "Gasolina", 180, 0, 20, "EEUU", "DEF234", "Automatica");
+        Carro c29 = new Carro("Kia", "Picanto", "Gris", "Gasolina", 160, 0, 14, "Corea", "GHI567", "Manual");
+        Carro c30 = new Carro("Chevrolet", "Tracker", "Azul", "Gasolina", 185, 0, 17, "EEUU", "JKL890", "Automatica");
+    }
+}
